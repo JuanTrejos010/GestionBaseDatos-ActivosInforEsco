0a9f003241d24e6459b64da976b29acdd38f9004
Juan Trejos <79339661+JuanTrejos010@users.noreply.github.com>
2025-10-29 14:10:29 -0500

    Create database.py

---
+++ database.py
@@ -0,0 +1,10 @@
+
+#Librerias de bases de datos
+import psycopg2
+
+#Condiciones de la conexión a la base de datos
+user = "root"
+#password = "A"
+host = "localhost"
+port = 5432
+#nombre_bd = "escolar"
