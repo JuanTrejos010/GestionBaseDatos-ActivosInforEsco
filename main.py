055225bd1b103f304ba181e580e827c951e273f8
BolshoyMaster <87585356+BolshoyMaster@users.noreply.github.com>
2025-10-31 00:53:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 from fastapi import FastAPI, HTTPException, Request, Form
 from fastapi.responses import RedirectResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
-import psycopg2
 import os
 import uvicorn
 
-#Plantillas
+#Llamando otros módulos
 from database import database
 
 #Se crean variables para las plantillas(templates)
 templates = Jinja2Templates(directory=".")
 app= FastAPI()
+
