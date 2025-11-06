cffd1a6ff4eae31af7269ff5d285e5c23f0163bd
BolshoyMaster <87585356+BolshoyMaster@users.noreply.github.com>
2025-11-06 17:01:39 -0500

    Cambios en main y database
    Se expandió para empezar a generar funciones

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 #Librerias de bases de datos
 import psycopg2
+import datetime
 
 #Condiciones de la conexión a la base de datos
 user = "postgres"
@@ -8,33 +9,59 @@ host = "localhost"
 port = 5432
 database = "Proyecto GestionBD"
 
-
 #Se crea la conexión
-try:
-    #Se pasa las condiciones a la función de conexión
-    conn=psycopg2.connect(
-        host=host,
-        user=user,
-        password=password,
-        port=port,
-        database=database
-    )
-    print(f"La base de datos {database} esta conectada.")
+def crear_Conexion():
+    try:
+        #Se pasa las condiciones a la función de conexión
+        conn=psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            port=port,
+            database=database
+        )
+        print(f"La base de datos {database} esta conectada.")
+        return conn
 
+    except Exception as Error:
+        print("Error en la conexión.")
+        return None
+
+def conexionPrueba(conn):
     #Se crea el cursor.
     cur=conn.cursor()
     cur.execute("SELECT version();")
     row=cur.fetchone()
     print(f"Version: \n{row}")
 
-except Exception as Error:
-    print("Error en la conexión.")
+#Funcion para crear tablas
+def crearTablas(conn):
+    cur=conn.cursor()
+    instruccion="""CREATE TABLE IF NOT EXISTS Equipo (
+        ID_Equipo SERIAL PRIMARY KEY,
+        Marca VARCHAR(100),
+        Modelo VARCHAR(100),
+        Fecha_compra DATE
+    );
+    """
+    cur.execute(instruccion)
+    cur.commit()
 
-'''
-Este finally esta oculto mientras se prueba cómo está la conexión a la base de datos
-finally:
-    #Se cierra la consulta
-    cur.close()
-    conn.close()
-    print("Conexion cerrada.")
-'''
+#Funcion para busquedas
+def buscarSalas(conn):
+    cur=conn.cursor()
+    cur.execute("SELECT Sala, Tipo FROM Sala")
+    rows=cur.fetchall()
+    for row in rows:
+        print(row)
+    return rows
+
+#Función para registrar un equipo
+def registrarEquipo(conn, marca, modelo):
+    cur=conn.cursor()
+    instruccion="""INSERT INTO Equipos (Marca, Modelo, Fecha_compra, )
+    VALUES
+        (%s, %s, %s, %s)
+    """
+    cur.execute(instruccion, {marca, modelo, datetime()})
+    cur.commit()
--- main.py
+++ main.py
@@ -6,17 +6,36 @@ import os
 import uvicorn
 
 #Llamando otros módulos
-from database import database
+from database import crear_Conexion
 
 #Se crean variables para las plantillas(templates)
 templates = Jinja2Templates(directory=".")
 app= FastAPI()
+conn=crear_Conexion()
 
-#Llamando los recursos
+"""
+
+Esta sección es para los recursos de frontend.
+
+"""
+
+#Llamando la raíz
 @app.get("/")
 def inicio(request: Request):
     return templates.TemplateResponse("inicio.html", {request: Request})
 
+"""
+
+Este es el listado de las tareas de CRUD como tal
+
+"""
+#Llamando 
+@app.get("Salas/")
+def tener_Salas():
+    print("Busqueda de salas")
+    
+    return "Busqueda de salas"
+
 #Ejecución del servidor
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8005)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
